57414e2844ae32b90347e0423948d7570b9c32fd
João Reinaldo <jreinaldo@abbc.org.br>
2025-10-02 15:39:49 -0300

    Feat: Criando repositorio do GHouse

---
+++ GHouse/src/main/java/com/GHouse/GHouse/GHouseApiApplication.java
@@ -0,0 +1,13 @@
+package com.GHouse.GHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GHouseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GHouseApiApplication.class, args);
+	}
+
+}
---
+++ GHouse/src/test/java/com/GHouse/GHouse/GHouseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GHouse.GHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GHouseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
