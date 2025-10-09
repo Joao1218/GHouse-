3a76734179f53299ec11a7242e0ceae92fcaad7c
Joao1218 <joaohmreinaldo@gmail.com>
2025-10-09 16:32:19 -0300

    Fix: arruma o mapper que transforma DTO em Entidade

--- GHouseApi/GHouseApi/src/main/java/com/GHouseApi/GHouseApi/mappers/userMapper.java
+++ GHouseApi/GHouseApi/src/main/java/com/GHouseApi/GHouseApi/mappers/userMapper.java
@@ -1,4 +1,15 @@
 package com.GHouseApi.GHouseApi.mappers;
 
+import com.GHouseApi.GHouseApi.dto.userDTO;
+import com.GHouseApi.GHouseApi.model.userModel;
+
 public class userMapper {
+
+    public static userModel dtoEmUsuario(userDTO userDTO){
+        return new userModel(null, userDTO.nome(), userDTO.emails(), null, null, userDTO.familias());
+    }
+
+    public static userDTO userEmDTO(userModel userModel){
+        return new userDTO(userModel.getNome(), userModel.getEmail(), userModel.getFamilias());
+    }
 }
