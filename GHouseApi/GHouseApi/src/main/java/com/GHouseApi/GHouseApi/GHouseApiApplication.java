0bc40c74f1fc7514fc723145a074ac8d1c9221d0
Joao1218 <joaohmreinaldo@gmail.com>
2025-10-13 16:46:24 -0300

    Fix: arrumando algumas anotações do spring

--- GHouseApi/GHouseApi/src/main/java/com/GHouseApi/GHouseApi/GHouseApiApplication.java
+++ GHouseApi/GHouseApi/src/main/java/com/GHouseApi/GHouseApi/GHouseApiApplication.java
@@ -2,8 +2,10 @@ package com.GHouseApi.GHouseApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GHouseApiApplication {
 
 	public static void main(String[] args) {
--- GHouseApi/GHouseApi/src/main/java/com/GHouseApi/GHouseApi/model/paymentsModel.java
+++ GHouseApi/GHouseApi/src/main/java/com/GHouseApi/GHouseApi/model/paymentsModel.java
@@ -3,6 +3,9 @@ package com.GHouseApi.GHouseApi.model;
 import com.GHouseApi.GHouseApi.enums.paymentsEnum;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -14,6 +17,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@EntityListeners(AuditingEntityListener.class)
 public class paymentsModel {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -22,7 +26,11 @@ public class paymentsModel {
     private UUID idUsuario;
     private UUID idConta;
     private UUID idFamilia;
+
+    @CreatedDate
     private LocalDate dataPagamento;
+
+    @LastModifiedDate
     private String comprovantePagamento;
     private paymentsEnum status;
 
