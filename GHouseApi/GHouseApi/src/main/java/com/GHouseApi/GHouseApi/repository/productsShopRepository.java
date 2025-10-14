5cb3e9e563fc751e1d95104b33b30381bd9c9e56
João Henrique <139931834+bkr6@users.noreply.github.com>
2025-10-13 23:05:27 -0300

    Fix: arrumando o nome e os parametros JPA da classe repository de productsShop

--- GHouseApi/GHouseApi/src/main/java/com/GHouseApi/GHouseApi/repository/productsShopRepository.java
+++ GHouseApi/GHouseApi/src/main/java/com/GHouseApi/GHouseApi/repository/productsShopRepository.java
@@ -1,8 +1,9 @@
 package com.GHouseApi.GHouseApi.repository;
 
+import com.GHouseApi.GHouseApi.model.productsShopModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface productsShopModel extends JpaRepository<productsShopModel, UUID> {
+public interface productsShopRepository extends JpaRepository<productsShopModel, UUID> {
 }
